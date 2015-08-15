1e2c99c840cc7bbc73861c4b45150da412de86a3
U-Undercity\Kelsay <krzysztof@the-earthworks.com>
2015-08-15 18:52:12 +0100

    [BE] Mapped Umbraco documents to menu items

---
+++ KelsayBackEnd/Controllers/API/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Kelsay.Models;
+using Kelsay.Code;
+
+namespace Kelsay.Controllers.API
+{
+    /// <summary>
+    /// Gets all menu items with appropriate data
+    /// </summary>
+    public class MenuController : MasterController
+    {
+
+        public HttpResponseMessage GetAll()
+        {
+            List<MenuModel> model = new List<MenuModel>();
+            IEnumerable<IPublishedContent> pages = Umbraco.GetRoot().Children();
+            if (pages.Any())
+            {
+                foreach (IPublishedContent page in pages)
+                {
+                    model.Add(new MenuModel()
+                    {
+                         Action = page.DocumentTypeAlias.ToLower(),
+                         Id = page.Id,
+                         Name = page.Name
+                    });
+                }
+            }
+            return Json(model);
+        }
+    }
+}
---
+++ KelsayBackEnd/Models/MenuModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kelsay.Models
+{
+    public class MenuModel
+    {
+        public string Name;
+        public int Id;
+        public string Action;
+    }
+}
