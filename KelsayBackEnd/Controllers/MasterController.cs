e04daea32ba3680c7a65941c381fbba141917eef
U-Undercity\Kelsay <krzysztof@the-earthworks.com>
2015-08-15 12:37:07 +0100

    [BE] User is now redirected from / to the back office

---
+++ KelsayBackEnd/Controllers/MasterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Training.Controllers
+{
+    public class HomeController : RenderMvcController
+    {
+        // Simple Redirect to the Back Office
+        public ActionResult Home()
+        {
+            return Redirect("/umbraco");
+        }
+    }
+}
