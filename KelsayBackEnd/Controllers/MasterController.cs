a6db4e25144cba71266ebf6331256cb18ecef465
U-Undercity\Kelsay <krzysztof@the-earthworks.com>
2015-08-15 12:38:33 +0100

    [BE] Changed default namespace

--- KelsayBackEnd/Controllers/MasterController.cs
+++ KelsayBackEnd/Controllers/MasterController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Umbraco.Web.Mvc;
 
-namespace Training.Controllers
+namespace Kelsay.Controllers
 {
     public class HomeController : RenderMvcController
     {
