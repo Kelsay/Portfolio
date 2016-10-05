d33fb4772d7cf72417837c9411a1b312534aad4a
Krzysztof Fijolek <fijolek.krzysztof@gmail.com>
2016-10-05 22:08:21 +0100

    [Header] Use new API service

---
+++ KelsayFrontEnd/app/components/header/header-controller.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    angular.module("App").controller("HeaderController", HeaderController);
+
+    HeaderController.$inject = ["API"];
+
+    function HeaderController(API) {
+
+        var vm = this;
+
+        function constructor() {
+            vm.items = API.getPage().$object;
+        }
+
+        constructor();
+    }
+
+})();
