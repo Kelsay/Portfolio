343f1cbc5f04d5d383b773b298770514196ef292
Krzysztof Fijolek <fijolek.krzysztof@gmail.com>
2016-10-05 22:14:02 +0100

    [App] App and project updates

--- KelsayFrontEnd/app/components/app.js
+++ KelsayFrontEnd/app/components/app.js
@@ -1,16 +1,34 @@
+/**
+ * The Main Application file
+ * Module declaration and some app wide settings
+ */
+
 'use strict';
-// The Main Application module
 
-(function () {
+var dependencies = [
+    'ngAnimate',
+    'restangular',
+    'ui.router',
+    'ui.bootstrap',
+    'ct.ui.router.extras.sticky'
+];
 
-    var app = angular.module("App", ['ngAnimate', 'restangular', 'ui.router', 'ui.bootstrap', 'ct.ui.router.extras.sticky'])
+var app = angular.module("App", dependencies)
 
     .config(['RestangularProvider', function (RestangularProvider) {
         RestangularProvider.setBaseUrl("http://api.fijolek.net")
     }])
 
-    .config(['$locationProvider', function ($locationProvider) {
-        $locationProvider.html5Mode(true);
+    // Default view for /
+    .config(['$urlRouterProvider', function ($urlRouterProvider) {
+        $urlRouterProvider.when("/", "/start/")
     }])
 
-})();
+    // Add base URL constant
+    .constant('ApiUrl', 'http://api.fijolek.net')
+
+    // Put the app in the HTML5 mode for the clean urls
+    .config(['$locationProvider', function ($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }]);
+
