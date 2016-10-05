3dd1fb2113e69dd08fd422a77392c8be80f617b5
Krzysztof Fijolek <fijolek.krzysztof@gmail.com>
2016-10-05 19:29:05 +0100

    [App] Added API service to replace Restangular

--- KelsayFrontEnd/app/components/api/api-method-factory.js
+++ KelsayFrontEnd/app/components/api/api-method-factory.js
@@ -2,8 +2,68 @@
 
     angular.module("App").factory("ApiMethodFactory", ApiMethodFactory);
 
-    function ApiMethodFactory() {
-        return "Dupa";
+    ApiMethodFactory.$inject = ["$http", "$q", "ApiUrl"];
+
+    function ApiMethodFactory($http, $q, ApiUrl) {
+        return {
+
+            /**
+             * Get new method
+             */
+            get: function (params) {
+
+                // Request parameters
+                params.url = ApiUrl + params.url;
+
+                // Adds an empty object for the reference
+                var helpers = {};
+                helpers.$object = {};
+                helpers.$list = [];
+
+                // Prepare request and handlers, bind helpers
+                var request = $http(params).then(successHandler.bind(helpers), errorHandler);
+
+                request.$object = helpers.$object;
+                request.$list = helpers.$list;
+
+                return function () {
+                    return request;
+                }
+            }
+        }
+
+        /**
+         * Default success handler
+         */
+        function successHandler(response) {
+
+            var data = response.data;
+            
+            if (angular.isArray(data)) {
+                for (var i in data) {
+                    var index = data[i].id || i;
+                    this.$object[index] = data[i];
+                    this.$list.push(data[i]);
+                }
+            } else if (response.data) {
+                angular.extend(this.$object, response.data);
+            }
+
+            // Adds non-enumerable property $loaded to help templating
+            Object.defineProperty(this.$object, "$loaded", { value: true, enumerable: false });
+            Object.defineProperty(this.$list, "$loaded", { value: true, enumerable: false });
+            
+            return response;
+        };
+
+        /**
+         * Default error handler
+         */
+        function errorHandler(response) {
+            console.error(response);
+            return $q.reject(response);
+        }
+
     }
 
-});
+})();
--- KelsayFrontEnd/app/components/api/api-service.js
+++ KelsayFrontEnd/app/components/api/api-service.js
@@ -2,38 +2,11 @@
 
     angular.module("App").service("API", ApiService);
 
-    ApiService.$inject = ["$http"];
+    ApiService.$inject = ["$http", "ApiMethodFactory", "ApiUrl"];
 
-    function ApiService($http) {
+    function ApiService($http, ApiMethodFactory, ApiUrl) {
 
-        /**
-         * Adds helper $object to the call results
-         * $object can be bound directly to the template and will be *then* populated on resolve
-         * Calls with only one result: $object will contain all the properties
-         * Calls with multiple results: $object will contain list of entities indexed by ID
-         * The $object contains also $loaded property to use within templates 
-        **/
-        this.addHelpers = function (response) {
-
-            var data = response.data.data;
-
-            if (angular.isArray(data)) {
-                for (var i in data) {
-                    var index = data[i].id || i;
-                    this.$object[index] = data[i];
-                    this.$list.push(data[i]);
-                }
-            } else if (response.data) {
-                angular.extend(this.$object, response.data);
-            }
-
-            // Adds non-enumerable property $loaded to help templating
-            Object.defineProperty(this.$object, "$loaded", { value: true, enumerable: false });
-            Object.defineProperty(this.$list, "$loaded", { value: true, enumerable: false });
-
-            return response;
-        };
-
+        this.getPages = ApiMethodFactory.get({ 'method': 'get', 'url': '/pages' });
 
     };
 
