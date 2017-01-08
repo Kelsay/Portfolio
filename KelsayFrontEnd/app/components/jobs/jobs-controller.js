566eb1b7da34f589ef70a26bc9201403ea118366
Krzysztof Fijolek <fijolek.krzysztof@gmail.com>
2017-01-08 17:09:22 +0000

    [FE] Cleanup

--- KelsayFrontEnd/app/components/jobs/jobs-controller.js
+++ KelsayFrontEnd/app/components/jobs/jobs-controller.js
@@ -8,26 +8,9 @@
 
         // Properties
         var vm = this;
-        vm.$showDetails = false;
 
         // Init
         vm.items = API.getJobs({ id: $stateParams.url }).$list;
-
-        /**
-         * Show details of the selected portfolio item
-         */
-        vm.showDetails = function (itemId) {
-            vm.details = {};
-            vm.details = API.getPortfolioById({ id: $stateParams.url, itemId: itemId }).$object;
-            vm.$showDetails = true;
-        }
-
-        /**
-         * Close the details
-         */
-        vm.hideDetails = function () {
-            vm.$showDetails = false;
-        }
 
     }
 
